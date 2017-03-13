fd8c11b5519ea5a41bac108df0381041dc780ddb
David Schoenbauer <dschoenbauer@coetruman.com>
2017-03-13 16:30:30 -0500

    Updated parameter visibility

---
+++ src/Orm/Events/Persistence/PdoCreate.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace DSchoenbauer\Orm\Events\Persistence;
+
+use DSchoenbauer\Orm\Events\AbstractEvent;
+use DSchoenbauer\Orm\Model;
+use DSchoenbauer\Sql\Command\Create;
+use DSchoenbauer\Sql\Where\ArrayWhere;
+use PDO;
+use Zend\EventManager\Event;
+
+/**
+ * Description of PdoCreate
+ *
+ * @author David Schoenbauer <dschoenbauer@gmail.com>
+ */
+class PdoCreate extends AbstractEvent
+{
+
+    private $adapter;
+    private $create;
+
+    public function __construct(PDO $adapter, Create $create= null)
+    {
+        $this->setAdapter($adapter)->setCreate($create);
+    }
+
+    public function onExecute(Event $event)
+    {
+        if (!$event->getTarget() instanceof Model) {
+            return;
+        }
+        /* @var $model Model */
+        $model = $event->getTarget();
+        $entity = $model->getEntity();
+        $this->getCreate()
+            ->setTable($entity->getTable())->setData($model->getData())
+            ->setWhere(new ArrayWhere([$entity->getIdField() => $model->getId()]))
+            ->execute($this->getAdapter());
+    }
+
+    public function getAdapter()
+    {
+        return $this->adapter;
+    }
+
+    public function setAdapter($adapter)
+    {
+        $this->adapter = $adapter;
+        return $this;
+    }
+
+    /**
+     * @return Create
+     */
+    public function getCreate()
+    {
+        if (!$this->create instanceof Create) {
+            $this->setCreate(new Create(null, []));
+        }
+        return $this->create;
+    }
+
+    public function setCreate($create)
+    {
+        $this->create = $create;
+        return $this;
+    }
+}
