c8750196d468b26c5e4fddd6246002653ff193c1
David Schoenbauer <dschoenbauer@coetruman.com>
2017-03-14 22:57:32 -0500

    added validate number

---
+++ src/Orm/Events/Validate/DataType/ValidateNumber.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace DSchoenbauer\Orm\Events\Validate\DataType;
+
+use DSchoenbauer\Orm\Entity\HasNumericFieldsInterface;
+
+/**
+ * Description of ValidateNumber
+ *
+ * @author David Schoenbauer <dschoenbauer@gmail.com>
+ */
+class ValidateNumber extends AbstractValidate
+{
+    
+    public function getFields($entity)
+    {
+        return $entity->getNumericFields();
+    }
+
+    public function getTypeInterface()
+    {
+        return HasNumericFieldsInterface::class;
+    }
+
+    public function validateValue($value)
+    {
+        return is_numeric($value);
+    }
+}
