a1cfe851364c2607b8987a2b52bfe12d8d4b4edc
David Schoenbauer <dschoenbauer@coetruman.com>
2017-03-15 05:07:51 -0500

    add validate string

---
+++ src/Orm/Events/Validate/DataType/ValidateString.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace DSchoenbauer\Orm\Events\Validate\DataType;
+
+/**
+ * Description of ValidateString
+ *
+ * @author David Schoenbauer <dschoenbauer@gmail.com>
+ */
+class ValidateString
+{
+    //put your code here
+}
